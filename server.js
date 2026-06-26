c3bd6fb1e0f44a4cc1b9cc3b98d18bb045f0a625
M.I. Paulo Daniel Vázquez Mora <73305449+danielvazquez-utp@users.noreply.github.com>
2026-06-26 16:31:38 +0000

    first commit

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+// Tareas básicas -CRUD -> Método
+// C - Create (Registro) -> POST
+// R - Read (Lectura/Recuperación) -> GET
+// U - Update (Actualización) -> PUT
+// D - Delete (Eliminación) -> DELETE
+
+// Ruta raíz
+app.get('/', (req, res) => {
+    res.json({
+        message: 'Bienvenidos a los servicios web del proyecto Biblioteca'
+    });
+});
+
+// Lanzamiento del servicio
+// visibilidad del puerto
+
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => {
+    console.log('Servidor esta corriendo en el puerto 4000');
+});
